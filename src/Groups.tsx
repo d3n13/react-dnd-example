1d4e682a5de1a8d581a87475a96822b8c541506a
Denis Bilenko <bilenkod@mail.ru>
2022-05-15 19:35:33 +0400

    refactor App & Groups

---
+++ src/Groups.tsx
@@ -0,0 +1,88 @@
+import { useState, useCallback } from "react";
+import { Group } from "./Group";
+import { moveArrayItemImmutable } from "./utils/moveArrayItem";
+import { isMoveDownImpossible, isMoveUpImpossible } from "./App.utils";
+import { Group as IGroup } from "./App.types";
+
+const initialGroups: IGroup[] = [
+  {
+    name: "Group 1",
+    items: [
+      {
+        name: "Group 1: Item 1",
+      },
+      {
+        name: "Group 1: Item 2",
+      },
+      {
+        name: "Group 1: Item 3",
+      },
+    ],
+  },
+  {
+    name: "Group 2",
+    items: [
+      {
+        name: "Group 2: Item 1",
+      },
+      {
+        name: "Group 2: Item 2",
+      },
+      {
+        name: "Group 2: Item 3",
+      },
+      {
+        name: "Group 2: Item 4",
+      },
+    ],
+  },
+  {
+    name: "Group 3",
+    items: [
+      {
+        name: "Group 3: Item 1",
+      },
+      {
+        name: "Group 3: Item 2",
+      },
+    ],
+  },
+];
+
+export function Groups() {
+  const [groups, setGroups] = useState<IGroup[]>(initialGroups);
+
+  const moveGroup = useCallback((index: number, toIndex: number) => {
+    setGroups((old) => moveArrayItemImmutable(old, index, toIndex));
+  }, []);
+
+  const moveItem = useCallback(
+    (groupIndex: number, index: number, toIndex: number) => {
+      setGroups((old) =>
+        old.map((oldGroup, oldGroupIndex) =>
+          oldGroupIndex === groupIndex
+            ? {
+                ...oldGroup,
+                items: moveArrayItemImmutable(oldGroup.items, index, toIndex),
+              }
+            : oldGroup
+        )
+      );
+    },
+    []
+  );
+
+  const renderGroup = (group: IGroup, index: number) => (
+    <Group
+      key={group.name}
+      group={group}
+      index={index}
+      moveGroup={moveGroup}
+      moveItem={moveItem}
+      moveDownDisabled={isMoveDownImpossible(groups, index)}
+      moveUpDisabled={isMoveUpImpossible(index)}
+    />
+  );
+
+  return <>{groups.map(renderGroup)}</>;
+}
